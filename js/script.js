081395e68eac84c16c6aa1d6eb3a135ba7545de3
Essence Fuller <essencefuller@Essences-MacBook-Pro.local>
2022-05-10 13:29:31 -0700

    Trying to get API properly linked

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+import { getCharacter } from 'rickmortyapi';
+
 //Variables
-const link = ""; //API Link
+const URL = "https://rickandmortyapi.com/api/character/"; //API Link
 
 //Element References
 const $name = $("#name");
@@ -9,23 +11,21 @@ const $form = $("form");
 const $input = $('input[type="text]');
 
 //Event Listeners
-$form.on('submit', getCharData)
-
+$form.on("submit", getCharData);
 
 //Functions
 function getCharData(event) {
-    event.preventDefault()
-    const userInput = $input.val()  //establishing/storing the value of the user input bar (so the user's actual input), into a variable
-   
-    $.ajax(link + userInput).then(function(data){
-        console.log(data);
-        $name.text(data.name)
-        $species.text(data.species)
-        $status.text(data.status)
-        $('main').append(`<img src = "${data.poster}"/>`) //COME BACK TO CHANGE TO API'S POSTER NAME
-    },
-    function (error){
-        console.log(error);
-    }
-}
+  event.preventDefault();
+  const userInput = $input.val(); //establishing/storing the value of the user input bar (so the user's actual input), into a variable
 
+  $.ajax(URL + userInput).then(function(data) {
+    console.log(data);
+    $name.text(data.name);
+    $species.text(data.species);
+    $status.text(data.status);
+    $("main").append(`<img src = "${data.image}"/>`); //COME BACK TO CHANGE TO API'S POSTER NAME
+  }),
+    function (error) {
+      console.log(error);
+    };
+}
