fe0e58530f174b401e5064ade15d32a058d46b68
Essence Fuller <essencefuller@Essences-MacBook-Pro.local>
2022-05-10 10:18:11 -0700

    Added js to make program functional and set up ajax. Need to get api link in and style. Still working on get git background working

--- js/script.js
+++ js/script.js
@@ -2,12 +2,30 @@
 const link = ""; //API Link
 
 //Element References
-const $name = $("name")
-const $species = $("species")
-const $status = $("status")
+const $name = $("#name");
+const $species = $("#species");
+const $status = $("#status");
+const $form = $("form");
+const $input = $('input[type="text]');
 
 //Event Listeners
+$form.on('submit', getCharData)
 
 
 //Functions
+function getCharData(event) {
+    event.preventDefault()
+    const userInput = $input.val()  //establishing/storing the value of the user input bar (so the user's actual input), into a variable
+   
+    $.ajax(link + userInput).then(function(data){
+        console.log(data);
+        $name.text(data.name)
+        $species.text(data.species)
+        $status.text(data.status)
+        $('main').append(`<img src = "${data.poster}"/>`) //COME BACK TO CHANGE TO API'S POSTER NAME
+    },
+    function (error){
+        console.log(error);
+    }
+}
 
