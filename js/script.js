e7cd712285c1bdef37732d724b17d49e3af9465a
Essence Fuller <essencefuller@Essences-MacBook-Pro.local>
2022-05-10 10:06:21 -0700

    created sections for character stats and stored them in jQuery variables

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Variables
+const link = ""; //API Link
+
+//Element References
+const $name = $("name")
+const $species = $("species")
+const $status = $("status")
+
+//Event Listeners
+
+
+//Functions
+
