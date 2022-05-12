a14646dc13317b629f508420de853df1ca289ea9
Essence Fuller <essencefuller@Essences-MacBook-Pro.local>
2022-05-12 07:21:01 -0700

    fixed search bar funcionality and submit button. Used submit method nd fixed prevent default function

--- js/script.js
+++ js/script.js
@@ -4,26 +4,39 @@
 const URL = "https://rickandmortyapi.com/api/character/2"; //API Link
 
 //Element References
-const $name = $("#name");
-const $species = $("#species");
-const $status = $("#status");
-const $form = $("form");
-const $input = $('input[type="text]');
+const $name = $("#name")
+const $species = $("#species")
+const $status = $("#status")
+const $form = $("form")
+const $input = $('input[type="text]')
 
 //Event Listeners
-$form.on("submit", getCharData);
+
+console.log($input)
+//$("#target").submit(getCharData);
+$form.submit(getCharData)
+
+function handler(event){
+    event.preventDefault()
+    console.log(event)
+    console.log($("#searchBar").val())
+}
 
 //Functions
-function getCharData() {
-  //event.preventDefault();
+function getCharData(event) {
+  //if (event) {
+    preventDefault()
+    console.log(event)
+  
+    console.log($("#searchBar").val())
   //const userInput = $input.val(); //establishing/storing the value of the user input bar (so the user's actual input), into a variable
 
-  $.ajax(URL).then(function(data) {
-    console.log(data);
-    $name.text(data.name);
-    $species.text(data.species);
-    $status.text(data.status);
-    $("main").append(`<img src = "${data.image}"/>`); //COME BACK TO CHANGE TO API'S POSTER NAME
+  $.ajax(URL).then(function (data) {
+    console.log(data)
+    $name.text(data.name)
+    $species.text(data.species)
+    $status.text(data.status)
+    $("main").prepend(`<img src = "${data.image}"/>`)
   }),
     function (error) {
       console.log(error);
