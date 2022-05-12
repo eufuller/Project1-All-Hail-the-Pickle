fdc61dfcbc6fd107150c5120f20cd54afc923785
Essence Fuller <essencefuller@Essences-MacBook-Pro.local>
2022-05-12 09:58:42 -0700

    played with search bar

--- js/script.js
+++ js/script.js
@@ -1,43 +1,48 @@
 //import { getCharacter } from 'rickmortyapi';
 
-//Variables
-const URL = "https://rickandmortyapi.com/api/character/?name="; //API Link
-
 //Element References
-const $name = $("#name")
-const $species = $("#species")
-const $status = $("#status")
-const $form = $("form")
-const $input = $('input[type="text]')
+const $name = $("#name");
+const $species = $("#species");
+const $status = $("#status");
+const $form = $("form");
+const $input = $('input[type="text]');
+const userInput = $input.val(); //establishing/storing the value of the user input bar (so the user's actual input), into a variable
+let searchBarInput = null;
+
+//Variables
+let URL = `https://rickandmortyapi.com/api/character/?name=${searchBarInput}`; //API Link
 
 //Event Listeners
 
-console.log($input)
+//console.log($input)
 //$("#target").submit(getCharData);
-$form.submit(getCharData)
+$form.submit(getCharData);
 
-function handler(event){
-    event.preventDefault()
-    console.log(event)
-    console.log($("#searchBar").val())
+function handler(event) {
+  event.preventDefault();
+  searchBarInput = $("#searchBar").val()
+  console.log(searchBarInput)
+  // console.log(event)
+  // console.log($("#searchBar").val())
 }
 
 //Functions
 function getCharData(event) {
   if (event) {
-    event.preventDefault()
-    console.log(event)
+    event.preventDefault();
+    //console.log(event)
+    console.log(URL);
   }
-  
-    console.log($("#searchBar").val())
-  //const userInput = $input.val(); //establishing/storing the value of the user input bar (so the user's actual input), into a variable
 
+  console.log($("#searchBar").val());
+  
   $.ajax(URL).then(function (data) {
-    console.log(data)
-    $name.text(data.name)
-    $species.text(data.species)
-    $status.text(data.status)
-    $("main").prepend(`<img src = "${data.image}"/>`)
+    // console.log(URL)
+    // console.log(data)
+    $name.text(data.name);
+    $species.text(data.species);
+    $status.text(data.status);
+    $("main").prepend(`<img src = "${data.image}"/>`);
   }),
     function (error) {
       console.log(error);
