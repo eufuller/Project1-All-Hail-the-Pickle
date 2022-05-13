65800341100256f8c4f7d9b4d9f4b7560e5368ce
Essence Fuller <essencefuller@Essences-MacBook-Pro.local>
2022-05-12 22:21:32 -0700

    Finished Product!

--- js/script.js
+++ js/script.js
@@ -4,56 +4,59 @@
 const $name = $("#name");
 const $species = $("#species");
 const $status = $("#status");
+const $type = $("#Type");
+const $gender = $("#gender");
 const $form = $("form");
 const $input = $('input[type="text"]');
 const userInput = $input.val(); //establishing/storing the value of the user input bar (so the user's actual input), into a variable
 let searchBarInput = null;
 
 //Variables
-let URL = "https://rickandmortyapi.com/api/character/2"; //API Link
-
+let URL = "https://rickandmortyapi.com/api/character/265"; //API Link to Pickle Rick
 
 //Event Listeners
-
-//console.log($input)
-//$("#target").submit(getCharData);
 $form.submit(getCharData);
 
-$("#target").submit(function(event){
-    event.preventDefault()
-    console.log($("#searchBar").val())
-})
-
-/*
-function handler(event) {
+$("#target").submit(function (event) {
   event.preventDefault();
-  searchBarInput = $("#searchBar").val()
-  console.log(searchBarInput)
-  // console.log(event)
-  // console.log($("#searchBar").val())
-}
-*/
+});
 
 //Functions
 function getCharData(event) {
   if (event) {
     event.preventDefault();
-    //console.log(event)
     console.log(URL);
   }
 
-  //console.log($("#searchBar").val());
-  
   $.ajax(URL).then(function (data) {
-    // console.log(URL)
-    // console.log(data)
+    console.log(data);
     $name.text(data.name);
     $species.text(data.species);
     $status.text(data.status);
-    $("main").prepend(`<img src = "${data.image}"/>`);
-    
+    $gender.text(data.gender);
+    $("main").append(`<img src = "${data.image}"/>`);
   }),
     function (error) {
       console.log(error);
     };
 }
+
+//Future Project: Element References
+//const $form = $("form");
+//const $input = $('input[type="text"]');
+//const userInput = $input.val(); //establishing/storing the value of the user input bar (so the user's actual input), into a variable
+//let searchBarInput = null;
+
+//Future Project: Event Listeners
+//console.log($input)
+//$("#target").submit(getCharData);
+//$form.submit(getCharData);
+//console.log($("#searchBar").val())
+
+//Future Project: Functions
+// function handler(event) {
+//     event.preventDefault();
+//     searchBarInput = $("#searchBar").val()
+//     console.log(searchBarInput)
+// console.log(event)
+// console.log($("#searchBar").val())
